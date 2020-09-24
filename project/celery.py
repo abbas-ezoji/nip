03e732e1a7dc216dbe10f1913534f1eaea182ab1
Abbas <Abbas.Ezoji@gmail.com>
2020-09-25 02:51:08 +0330

    [EDIT] Refactor to celery by redis

--- project/celery.py
+++ project/celery.py
@@ -8,7 +8,7 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
-app = Celery("celery_project")
+app = Celery("project")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
