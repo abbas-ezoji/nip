a5326c1f0cbd58d6e3f61417d4226fb4c1d0d4b0
Abbas <Abbas.Ezoji@gmail.com>
2022-03-03 13:37:12 +0330

    [EDIT] Dash app and Auth

--- nip/views.py
+++ nip/views.py
@@ -7,8 +7,11 @@ from django.http import HttpResponse
 def model(request, pk):
     user = request.user
     print(user)
+    context = {'data': {'input': {'value': pk}}}
     if user.is_anonymous:
         return HttpResponse('شما به این شیفت دسترسی ندارین ... !', status=403)
+    if user.is_superuser:
+        return render(request, 'nip/table.html', context)
 
     profile = UserProfile.objects.get(User=user)
     user_work_sec = profile.WorkSection
@@ -16,10 +19,9 @@ def model(request, pk):
     shift_ass = ShiftAssignments.objects.get(pk=assignment_id)
     shift_work_sec = shift_ass.WorkSection
 
-    print(f'user_work_sec = {user_work_sec.id}, shift_work_sec = {shift_work_sec.id}')
+    # print(f'user_work_sec = {user_work_sec.id}, shift_work_sec = {shift_work_sec.id}')
 
-    if not user.is_superuser and user_work_sec.id != shift_work_sec.id:
+    if user_work_sec.id != shift_work_sec.id:
         return HttpResponse('شما به این شیفت دسترسی ندارین ... !', status=401)
 
-    context = {'data': {'input': {'value': pk}}}
     return render(request, 'nip/table.html', context)
