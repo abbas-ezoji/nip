3b6bfd2b43309743e4e5eca74892a631db91d8b3
Abbas <Abbas.Ezoji@gmail.com>
2022-05-01 00:41:27 +0430

    [EDIT] Dash app by prs and days info

--- project/dash_apps/simple.py
+++ project/dash_apps/simple.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import copy
+import time
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
@@ -12,6 +12,57 @@ import dash_table
 from nip.training.data_access.db import get_db
 from sqlalchemy import create_engine
 
+
+def get_zeropad(i, n):
+    str_i = str(i)
+    zeropad = ''
+    for i in range(n - len(str_i)):
+        zeropad = zeropad + '0'
+    numzeropad = zeropad + str_i
+
+    return numzeropad
+
+
+def get_day_info_df(df):
+    day_info_cols = [col for col in df.columns]
+    day_info_cols[0] = 'DayStatistics'
+    day_info_df = pd.DataFrame([], columns=day_info_cols)
+    day_info_df.at[:, 'DayStatistics'] = ['M', 'A', 'N']
+
+    for col in df.columns:
+        if col == 'FullName' or df.loc[0, col] is None:
+            continue
+        day_info = np.array((df.loc[:, col]).values)
+        # print(day_info)
+        M = len(day_info[np.logical_or(day_info // 10 == 1, day_info % 10 == 1)])
+        A = len(day_info[np.logical_or(day_info // 10 == 2, day_info % 10 == 2)])
+        N = len(day_info[np.logical_or(day_info // 10 == 3, day_info % 10 == 3)])
+        day_info_df.at[:, col] = [M, A, N]
+
+    return day_info_df
+
+
+def get_prs_info_df(df):
+    sum_shift_len = []
+    for i, row in df.iterrows():
+        prs_name = row['FullName']
+        sum_shifts = 0
+        for col in df.columns:
+            if col == 'FullName' or col == 'SumShift':
+                continue
+            sh_id = df.loc[i, col]
+            if sh_id is None:
+                continue
+            sh_lenght = shifts_df[shifts_df['id'] == sh_id]['Length'].values[0]
+            sum_shifts += sh_lenght
+        sum_shifts_t = time.strftime('%H:%M:%S', time.gmtime(sum_shifts))
+        sum_shift_len.append([prs_name, sum_shifts])
+
+    sum_shift_len = np.array(sum_shift_len)
+    df_sum_shift_len = pd.DataFrame(sum_shift_len, columns=['FullName', 'sum'])
+    return df_sum_shift_len
+
+
 # ----------------------- get data -------------------------------------------#
 DATABASES = get_db()
 USER = DATABASES['nip']['USER']
@@ -22,6 +73,25 @@ NAME = DATABASES['nip']['NAME']
 
 con_string = f'mssql+pyodbc://{USER}:{PASSWORD}@{HOST}/{NAME}?driver=SQL+Server'
 engine = create_engine(con_string)
+
+query_shifts = '''
+SELECT [id]
+      ,[Code]
+      ,[Title]
+      ,[Length]
+      ,[StartTime]
+      ,[EndTime]
+      ,[Type]
+      ,[ExternalGuid]
+      ,[ExternalId]
+  FROM [nip].[dbo].[nip_shifts]
+'''
+
+shifts_df = pd.read_sql(query_shifts, engine)
+shifts = shifts_df['id'].values
+df_colors = pd.DataFrame(data=dict(COLOR=['#1f77b4', '#d62728', '#e377c2', '#17becf', '#bcbd22'],
+                                   VALUE=[1, 2, 3, 12, 13]))
+
 query = '''
 SELECT 
 	  p.FullName
@@ -61,64 +131,73 @@ SELECT
   where ShiftAssignment_id = {} or 0 = {}
 '''
 df = pd.read_sql(query.format(4296, 1), engine)
+prs_info_df = get_prs_info_df(df)
 
-PAGE_SIZE = 30
-
-query_shifts = '''
-SELECT [id]
-      ,[Code]
-      ,[Title]
-      ,[Length]
-      ,[StartTime]
-      ,[EndTime]
-      ,[Type]
-      ,[ExternalGuid]
-      ,[ExternalId]
-  FROM [nip].[dbo].[nip_shifts]
-'''
-
-shifts_df = pd.read_sql(query_shifts, engine)
-shifts = shifts_df['id'].values
-df_colors = pd.DataFrame(data=dict(COLOR=['#1f77b4', '#d62728', '#e377c2', '#17becf', '#bcbd22'],
-                                   VALUE=[1, 2, 3, 12, 13]))
-
-import time
-
-
-def add_sum_shift_len(df):
-    sum_shift_len = []
-    for row in range(len(df)):
-        sum_shifts = 0
-        for col in df.columns:
-            if col == 'FullName':
-                continue
-            sh_id = df.loc[row, col]
-            if sh_id is None:
-                continue
-            sh_lenght = shifts_df[shifts_df['id'] == sh_id]['Length'].values[0]
-            sum_shifts += sh_lenght
-        sum_shifts_t = time.strftime('%H:%M:%S', time.gmtime(sum_shifts))
-        sum_shift_len.append(sum_shifts_t)
-
-    sum_shift_len = np.array(sum_shift_len)
-    df_sum_shift_len = pd.DataFrame(sum_shift_len, columns=['SumShift'])
-    df_sum_shift_len['FullName'] = df['FullName']
-    return df_sum_shift_len
-
-
-df_sum_shift_len = add_sum_shift_len(df)
+day_info_df = get_day_info_df(df)
 
 fig = px.bar(df, x="FullName", y="D01", barmode="group")
-
-table_header = [
-    html.Thead(html.Tr([html.Th("First Name"), html.Th("Last Name")]))
-]
-
-# import sd_material_ui
 
 app = DjangoDash('simple',
                  meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1.0"}]
                  )
+
+table_shift = dash_table.DataTable(
+    id='table_shift',
+    columns=[
+        {"name": col, "id": col, 'presentation': 'dropdown'} if col != 'FullName' and col != 'SumShift'
+        else {"name": col, "id": col} for i, col in enumerate(df.columns)
+    ],
+    data=df.to_dict('records'),
+    dropdown={col: {
+        'options': [
+            {'label': shift['Title'], 'value': shift['id']}
+            for i, shift in shifts_df.iterrows()
+        ]
+    } if col != 'FullName' and col != 'SumShift' else {} for i, col in enumerate(df.columns)
+              },
+    style_cell={'textAlign': 'center', 'maxWidth': 0},
+    style_data_conditional=[
+        {
+            'if': {
+                'column_id': 'D' + get_zeropad(i, 2)
+            },
+            'backgroundColor': 'dodgerblue',
+            # 'if': {
+            #     'column_id': 'FullName'
+            # },
+            # 'backgroundColor': 'green',
+            'color': 'white'
+        } for i, col in enumerate(df.columns)
+    ],
+    editable=True,
+)
+
+table_prs_info = dash_table.DataTable(
+    id='table_prs_info',
+    columns=[
+        {"name": col, "id": col} for i, col in enumerate(prs_info_df.columns)
+    ],
+    data=prs_info_df.to_dict('records'),
+    style_cell={'textAlign': 'center', 'maxWidth': 0},
+    style_data_conditional=[
+        {
+            'if': {
+                'row_index': i
+            },
+            'backgroundColor': 'red',
+            'color': 'white'
+        } if int(row[1]) > 18000 else {} for i, row in prs_info_df.iterrows()
+    ],
+)
+
+table_day_info = dash_table.DataTable(
+    id='table_day_info',
+    columns=[
+        {"name": col, "id": col} for i, col in enumerate(day_info_df.columns)
+    ],
+    data=day_info_df.to_dict('records'),
+    style_cell={'textAlign': 'center', 'maxWidth': 0},
+),
 
 app.layout = html.Div([
     dcc.Input(
@@ -127,39 +206,14 @@ app.layout = html.Div([
         value=1,
         type="hidden"
     ),
-    dash_table.DataTable(
-        id='table_sum',
-        columns=[
-            {"name": col, "id": col} for i, col in enumerate(df_sum_shift_len.columns)
-        ],
-        data=df_sum_shift_len.to_dict('records'),
-        style_cell={'textAlign': 'center'},
-    ),
-    html.Button('تایید و انتقال به دیدگاه', id='submit-val', n_clicks=0),
-    dash_table.DataTable(
-        id='table',
-        columns=[
-            {"name": col, "id": col, 'presentation': 'dropdown'} if col != 'FullName' and col != 'sum_shift_len'
-            else {"name": col, "id": col} for i, col in enumerate(df.columns)
-        ],
-        data=df.to_dict('records'),
-        dropdown={col: {
-            'options': [
-                {'label': shift['Title'], 'value': shift['id']}
-                for i, shift in shifts_df.iterrows()
-            ]
-        } if col != 'FullName' and col != 'sum_shift_len' else {} for i, col in enumerate(df.columns)
-                  },
-        style_cell={'textAlign': 'center', 'maxWidth': 0},
-        style_data=[
-            {'if': {'row_index': i, 'column_id': 'D01'}, 'background-color': df_colors['COLOR'][i],
-             'color': df_colors['COLOR'][i]}
-            for i in range(df_colors.shape[0])],
-        editable=True,
-        # page_current=0,
-        # page_size=PAGE_SIZE,
-        # page_action='custom'
-    ),
+    html.Table([
+        html.Tr([html.Td(table_shift),
+                 html.Td(table_prs_info)
+                 ]),
+        html.Tr([html.Td(table_day_info)])
+
+    ], style={'width': '100%'}),
+
     dcc.Graph(
         id='chart-output',
         figure=fig
@@ -168,7 +222,7 @@ app.layout = html.Div([
 
 
 @app.callback(
-    Output('table', 'data'),
+    Output('table_shift', 'data'),
     [Input('input', 'value'), ]
 )
 def change_output(value):
@@ -183,26 +237,39 @@ def change_output(value):
 
 @app.callback(
     Output('chart-output', 'figure'),
-    [Input('table', 'data'),
-     Input('table', 'columns')])
+    [Input('table_shift', 'data'),
+     Input('table_shift', 'columns')])
 def display_chart(rows, columns):
     df = pd.DataFrame(rows, columns=[c['name'] for c in columns])
-    # df = add_sum_shift_len(df)
     fig = px.bar(df, x="FullName", y="D01", barmode="group")
-
+    print('display_chart')
     return fig
 
 
 @app.callback(
-    Output('table_sum', 'data'),
-    [Input('table', 'data'),
-     Input('table', 'columns')])
-def display_chart(rows, columns):
+    Output('table_prs_info', 'data'),
+    [Input('table_shift', 'data'),
+     Input('table_shift', 'columns')])
+def change_shift_to_prs_info(rows, columns):
     df = pd.DataFrame(rows, columns=[c['name'] for c in columns])
-    df_sum_shift_len = add_sum_shift_len(df)
+    df = get_prs_info_df(df)
     data = [
-        dict(Model=i, **{param: df_sum_shift_len.loc[i, param] for param in df_sum_shift_len.columns})
-        for i in range(len(df_sum_shift_len))
+        dict(Model=i, **{param: df.loc[i, param] for param in df.columns})
+        for i in range(len(df))
+    ]
+
+    return data
+
+@app.callback(
+    Output('table_day_info', 'data'),
+    [Input('table_shift', 'data'),
+     Input('table_shift', 'columns')])
+def change_shift_to_days_info(rows, columns):
+    df = pd.DataFrame(rows, columns=[c['name'] for c in columns])
+    df = get_day_info_df(df)
+    data = [
+        dict(Model=i, **{param: df.loc[i, param] for param in df.columns})
+        for i in range(len(df))
     ]
 
     return data
